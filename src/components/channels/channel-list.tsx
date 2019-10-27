96e53a75ddf99b67370d91b35451c5337d427540
Izel Aciman <izelaciman@gmail.com>
2019-10-27 22:10:21 +0000

    remove telegraph.

--- src/components/channels/channel-list.tsx
+++ src/components/channels/channel-list.tsx
@@ -7,7 +7,7 @@ const ChannelsField: React.FC = () => (
     <Channel mode="sources" channelName="CNN" channelString="cnn" />
     <Channel mode="sources" channelName="CNBC" channelString="cnbc" />    
     <Channel mode="sources" channelName="Business" channelString="business-insider-uk" /> 
-    <Channel mode="sources" channelName="Telegraph" channelString="the-telegraph" />    
+    {/* <Channel mode="sources" channelName="Telegraph" channelString="the-telegraph" />     */}
     <Channel mode="sources" channelName="JPost" channelString="the-jerusalem-post" />
     <Channel mode="country" channelName="Turkey" channelString="tr" />
     <Channel mode="country" channelName="Israel" channelString="il" />
